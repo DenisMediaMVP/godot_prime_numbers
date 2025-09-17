349334c946f5ff72b44969b10232f21733604554
DenisMVP <denisvergara@outlook.com>
2025-09-16 22:04:46 -0300

    Update prime_numbers.gd

--- prime_numbers.gd
+++ prime_numbers.gd
@@ -19,6 +19,7 @@ var primes := PackedInt32Array([2])
 
 
 func primes_up_to(limit: int,new_thread:Thread) -> void:
+	var thread_calculation_t0 : float = Time.get_ticks_msec()
 	
 	if limit < 2:
 		return PackedInt32Array()
@@ -42,31 +43,32 @@ func primes_up_to(limit: int,new_thread:Thread) -> void:
 	
 	if new_thread:
 		var thread_id = new_thread.get_id()
+		var thread_calculation_final : float = (Time.get_ticks_msec() - thread_calculation_t0) / 1000
+		print(" THREAD ID = " + str(thread_id) + "tiempo de calculado = " + str(thread_calculation_final))
 		
-
-
-	
-
-
+	if not new_thread:
+		var thread_calculation_final : float = (Time.get_ticks_msec() - thread_calculation_t0) / 1000
+		print(" MAIN THREAD  = " + "tiempo de calculado = " + str(thread_calculation_final))
+		
 func _enter_tree() -> void:
 	for thread in threads:
 		thread.wait_to_finish()
 
 
 func _on_start_main_pressed() -> void:
-	var time_elapsed : float 
+	var total_time : float 
 	var t0:float = Time.get_ticks_msec()
 	
 	for i in 10:
 		var new_thread = null
 		primes_up_to(limit,new_thread)
-	time_elapsed = (Time.get_ticks_msec() - t0) / 1000
+	total_time = (Time.get_ticks_msec() - t0) / 1000
 		
-	print("MAIN THREAD TOTAL TIME = " + str(time_elapsed))
+	print("MAIN THREAD TOTAL TIME = " + str(total_time))
 
 
 func _on_start_prime_search_pressed() -> void:
-	var time_elapsed : float 
+	var total_time : float 
 	var t0:float = Time.get_ticks_msec()
 	
 	for i in number_of_thread:
@@ -78,5 +80,5 @@ func _on_start_prime_search_pressed() -> void:
 	for thread in threads:
 		thread.wait_to_finish()
 		
-	time_elapsed = (Time.get_ticks_msec() - t0) / 1000
-	print("THREAD TOTAL TIME = " + str(time_elapsed))
+	total_time = (Time.get_ticks_msec() - t0) / 1000
+	print("THREAD TOTAL TIME = " + str(total_time))
